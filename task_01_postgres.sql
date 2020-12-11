1b2c206d9d9e8a56e3d7ffc9a219e20f5b7a3c73
Alex Shtilman <gungam@outlook.com>
2020-12-11 20:40:10 +0200

    first commit

---
+++ task_01_postgres.sql
@@ -0,0 +1,44 @@
+
+CREATE TABLE IF NOT EXISTS products(
+    productID    serial NOT NULL,
+    productCode  CHAR(3)       NOT NULL DEFAULT '',
+    name         VARCHAR(30)   NOT NULL DEFAULT '',
+    quantity     INT   NOT NULL DEFAULT 0,
+    price        DECIMAL(7,2)  NOT NULL DEFAULT 99999.99,
+    PRIMARY KEY  (productID)
+);
+
+DROP TABLE IF EXISTS products;
+
+INSERT INTO products VALUES 
+(1004, 'PEX', 'Pen Red', 5000, 0.48),
+(1005, 'PEX', 'Pen Blue', 1000, 3.23),
+(1006, 'PEX', 'Pen Black', 3000, 5.23),
+(1002, 'PEC', 'Pencil 2B', 1000, 0.49),
+(1003, 'PEC', 'Pencil 2H', 10000, 3.23);
+
+
+DELETE FROM products WHERE productID > 1005;
+
+SELECT * FROM products;
+
+DROP TABLE IF EXISTS weekdays;
+
+CREATE TABLE IF NOT EXISTS weekdays(
+    weekID    serial NOT NULL,
+    name  VARCHAR(9) NOT NULL DEFAULT '',
+    PRIMARY KEY  (weekID)
+);
+
+INSERT INTO weekdays VALUES 
+(1, 'Sunday'),
+(2,	'Monday'),
+(3,	'Tuesday'),
+(4,	'Wednesday'),
+(5,	'Thursday'),
+(6,	'Friday'),
+(7,	'Saturday');
+
+SELECT * FROM weekdays;
+
+SELECT * FROM weekdays WHERE CHAR_LENGTH(name) < 7;
---
+++ task_02_mysql.sql
@@ -0,0 +1,20 @@
+-- 1
+SELECT name, quantity from products where productCode ='PEC';
+-- 2
+SELECT productID FROM products where name regexp '(^|[[:space:]])Blue([[:space:]]|$)';
+-- 3
+SELECT name FROM products where name LIKE '%n%' AND quantity > 9000
+-- 4
+SELECT name,price FROM products where quantity between 4000 and 9000 ORDER BY name ASC;
+-- 5
+SELECT * from products where price IN (0.48, 0.49 , 1.25)
+-- 6
+SELECT name, quantity,price from products ORDER BY price DESC LIMIT 3;
+-- 7
+SELECT 
+    CONCAT(productCode,"-", name) AS "Product Description",
+    price as "Item Price",
+    quantity as "Quantity",
+    (price * quantity) as "Cost" 
+        FROM products 
+            ORDER BY 4 DESC;
